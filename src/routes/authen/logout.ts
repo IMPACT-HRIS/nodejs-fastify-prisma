fb187582ce056d6a7d516566839553ac50795754
Karis <kariz.matchaparn@gmail.com>
2022-11-30 17:04:43 +0700

    feat: destroy cookie on logout

--- src/routes/authen/logout.ts
+++ src/routes/authen/logout.ts
@@ -1,8 +1,11 @@
 import { FastifyError, FastifyInstance, RegisterOptions } from 'fastify'
+import { ENV_SESSION_COOKIE_PATH } from '~/config/env'
+import { SESSION_COOKIE_NAME } from '~/config/session'
 
 export default (app: FastifyInstance, opts: RegisterOptions, done: (err?: FastifyError) => void) => {
   app.post('/authen/logout', (request, reply) => {
     request.session.destroy(() => {
+      reply.setCookie(SESSION_COOKIE_NAME, '', { maxAge: -1, path: ENV_SESSION_COOKIE_PATH })
       reply.send()
     })
   })
